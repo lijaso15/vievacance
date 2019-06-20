ff3addaeb9a8de79ba8334f0db7900abb8a803ae
Jason <jasonlimale.li@mail.utoronto.ca>
2019-06-20 15:47:00 -0400

    trying to fix a stupid bug

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (!isDev && cluster.isMaster) {
         secret: 'oursecret',
         resave: false,
         cookie: {
-            expires: 600000,
+            expires: 600000 * 3,
             secure: true
         },
         saveUninitialized: false
