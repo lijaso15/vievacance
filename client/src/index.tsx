a01b75c6a40eb33b647f496dcf259036f6b7ff69
Jason <jasonlimale.li@mail.utoronto.ca>
2019-07-27 18:04:54 -0400

    installing packages...

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
-import rootReducer from './reducers'
-import App from './App'
-import './index.css'
+import rootReducer from "./reducers";
+import App from "./App";
+import "./index.css";
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
 render(
-    // the store glues together the states of the entire application
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')
-)
+  // the store glues together the states of the entire application
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
